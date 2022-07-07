060e81ec8beac974c70c3f6f769bf2d6e74a46aa
Tharindu Yalegama <87438005+tharinduyalegamalucky@users.noreply.github.com>
2022-07-06 20:35:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let age = 30;
+console.log(age);
+
+let score;
+score = 10;
+console.log(score);
+
+//String , Numbers, Boolean, null, undefined
+
+const name = 'John';
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
