be6d0fad9261562a4d324575458a30ae49335cdc
Tharindu Yalegama <87438005+tharinduyalegamalucky@users.noreply.github.com>
2022-08-18 09:46:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -204,3 +204,105 @@ const d = 11;
 if(a > 5 && d > 10) {
     console.log('a is more than 5 or d is more than 10');
 }
+
+
+// ------BMI Comparison-------
+
+// var mass, height, bmik, bmic, isKasun;
+
+// // Kasun's BMI
+// mass = prompt("Kasun's mass : ");
+// height = prompt("Kasun's height : ");
+// bmik = mass / (height * height);
+
+// //Chamar's BMI
+// mass = prompt("Chamara's mass : ");
+// height = prompt("Chamara's height : ");
+// bmic = mass / (height * height);
+
+// //boolean
+// isKasun = (bmik > bmic);
+
+// alert(isKasun);
+
+
+// If Else----------
+// -----------------
+
+// var a = 10;
+// var b = 20;
+
+// if(a > b){
+//     console.log("a is greater than b");
+// }
+// else if(a < b){
+//     console.log("a less than b");
+// }
+// else{
+//     console.log("a is less than or equal to b");
+// }
+
+// if(a == 10){
+//     console.log("a is equal to 10");
+// }
+
+
+// --------------------------------------
+// ---------Comparison Operators---------
+// --------------------------------------
+
+var a = 20;
+var b = '20';
+var c = 5;
+var d = 15;
+
+//Equal to
+if(a === b){
+    // Equal value and equal type
+    if(a === b){
+        console.log('Both value and type of a is equal to b');
+    }
+    // Not Equal value or not equal type
+    else if(a !== b){
+        console.log('Only value or type of a is equal to b');
+    }
+}
+
+// Greater than oe equal
+else if(a == 10){
+    // greater than
+    if(a > 10){
+        console.log('a is greater than 10')
+    }
+    //equal to
+    else if(a == 10){
+        console.log('a is equal to 10');
+    }
+}
+
+// Not equal
+else if(a == c){
+    // Less than
+    if(a < c){ 
+        console.log('a is less than c');
+    }
+    else{
+        console.log('a is greater than c');
+    }
+}
+
+else {
+    // less than or equal to
+    if(c <= d){
+        // if(c <= 5){
+        //     console.log('c is less than or equal to 5');
+        // }
+        // else{
+        //     console.log('c is greater than to 5');
+        // }
+
+        //  Ternary Operator
+        c <= 5 ? console.log('c is less than or equal to 5')  : 
+        console.log('c is greater than to 5');
+    }
+}
